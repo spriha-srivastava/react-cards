bd9bab2727693e7509c6fa11000fc31a70f1fc7e
Spriha Srivastava <srivastavaspriha18@gmail.com>
2024-08-02 21:25:43 +0530

    added one card

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <Header name="Spriha" age="22" address="Patna" course="Btech" />
       <Header name="Kajal" age="22" address="Mumbai" course="Btech" />
       <Header name="Palak" age="21" address="Delhi" course="Btech" />
-      <Header name="Utsav" age="22" address="Kanpur" course="Btech" />
+      <Header name="Ashish" age="22" address="Kanpur" course="Btech" />
+      <Header name="Aaryan" age="21" address="Lucknow" course="Btech" />
     </div>
 
   );
